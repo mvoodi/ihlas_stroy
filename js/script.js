712380e1518c377b9f9e08df02f7de52102cdc60
Aleksandra Arykova <aleksandra.arykova@alatoo.edu.kg>
2024-05-12 17:21:47 +0600

    add adaptation

--- js/script.js
+++ js/script.js
@@ -1,34 +1,40 @@
-const navButtons = document.querySelectorAll('.nav-links button');
+  const menuBtn = document.querySelector('.menu-btn');
+  const navlinks = document.querySelector('.nav-links');
 
-// Добавляем обработчик события для каждой кнопки
-navButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        // Получаем URL страницы из атрибута data-url кнопки
-        const url = button.getAttribute('data-url');
+  menuBtn.addEventListener('click', () => {
+      navlinks.classList.toggle('mobile-menu');
+  });
 
-        // Перенаправляем пользователя на указанный URL
-        window.location.href = url;
-    });
+  const navButtons = document.querySelectorAll('.nav-links button');
+
+  navButtons.forEach(button => {
+      button.addEventListener('click', () => {
+          const url = button.getAttribute('data-url');
+          window.location.href = url;
+      });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const textElement = document.querySelector('.text');
-    const originalText = 'The mission if Ihlas Company is to work, creating a better quality of life for people! IHLAS is a group of companies providing a wide range of services in the construction market of Kyrgyzstan, Kazakhstan and Russia. The main activity of the company is the design, construction and sale of residential and commercial real estate.';
-    const newText = 'For 21 years of work in the construction market of Kyrgyzstan, Ihlas has successfully completed the construction of 16 multi-storey buildings and other facilities. The total number of commissioned apartments was 1,437, and the number of commissioned parking spaces was 410. Our own concrete production is 1 million cubic meters per year, and the production of screening and crushed stone is 2.0 million tons per year.';
-  
-    let scrolled = false;
-  
-    window.addEventListener('scroll', function() {
-      const scrollPosition = window.scrollY;
-  
-      if (scrollPosition > 100 && !scrolled) {
-        textElement.innerHTML = `<p>${newText}</p>`;
-        scrolled = true;
-      } else if (scrollPosition <= 100 && scrolled) {
-        textElement.innerHTML = `<p>${originalText}</p>`;
-        scrolled = false;
-      }
-    });
-  });
-  
-        
+const serviceBtn = document.getElementById("service");
+serviceBtn.addEventListener('click', () => {
+  window.location.href = 'bestseller.html';
+});
+
+const trailsBtn = document.getElementById("trails");
+trailsBtn.addEventListener('click', () => {
+  window.location.href = 'about.html';
+});
+
+const featureBtn = document.getElementById("feature");
+featureBtn.addEventListener('click', () => {
+  window.location.href = 'feature.html';
+});
+
+const contactBtn = document.getElementById("contact");
+contactBtn.addEventListener('click', () => {
+  window.location.href = 'contact.html';
+});
+
+const homeBtn = document.getElementById("home");
+homeBtn.addEventListener('click', () => {
+  window.location.href = 'index.html';
+});
